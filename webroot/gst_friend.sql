01f63fb0f7be236d3d4b2635aae6e514ebb9a765
Abhilash Lohar <abhilashlohar01@gmail.com>
2017-07-28 11:53:33 +0530

    adasd

--- webroot/gst_friend.sql
+++ webroot/gst_friend.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 27, 2017 at 02:38 PM
--- Server version: 10.1.21-MariaDB
--- PHP Version: 5.6.30
+-- Generation Time: Jul 28, 2017 at 08:22 AM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 7.0.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,6 +19,38 @@ SET time_zone = "+00:00";
 --
 -- Database: `gst_friend`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accounting_groups`
+--
+
+CREATE TABLE `accounting_groups` (
+  `id` int(10) NOT NULL,
+  `nature_of_group_id` int(10) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `parent_id` int(11) DEFAULT NULL,
+  `lft` int(10) DEFAULT NULL,
+  `rght` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `accounting_groups`
+--
+
+INSERT INTO `accounting_groups` (`id`, `nature_of_group_id`, `name`, `parent_id`, `lft`, `rght`) VALUES
+(1, 2, 'Branch / Divisions', NULL, 1, 2),
+(2, 2, 'Capital Account', NULL, 3, 6),
+(3, 0, 'Reserves & Surplus', 2, 4, 5),
+(4, 1, 'Current Assets', NULL, 7, 12),
+(5, 0, 'Bank Accounts', 4, 8, 9),
+(6, 0, 'Cash-in-hand', 4, 10, 11),
+(7, 2, 'Current Liabilities', NULL, 13, 18),
+(8, 4, 'Direct Expenses', NULL, 19, 20),
+(9, 3, 'Direct Incomes', NULL, 21, 22),
+(10, 0, 'Duties & Taxes', 7, 14, 17),
+(11, 0, 'Output GST', 10, 15, 16);
 
 -- --------------------------------------------------------
 
@@ -95,7 +127,29 @@ CREATE TABLE `items` (
 
 INSERT INTO `items` (`id`, `name`, `hsn_code`, `freezed`, `company_id`) VALUES
 (1, 'stl', '123456', 1, 1),
-(2, 'anil', 'fjeoifjwe90324', 0, 1);
+(2, 'anil', 'fjeoifjwe90324', 0, 1),
+(3, 'safsa', '', 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ledgers`
+--
+
+CREATE TABLE `ledgers` (
+  `id` int(10) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `accounting_group_id` int(10) NOT NULL,
+  `freeze` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `ledgers`
+--
+
+INSERT INTO `ledgers` (`id`, `name`, `accounting_group_id`, `freeze`) VALUES
+(1, 'test ledger 1', 4, 0),
+(2, 'SBBJ Bank', 5, 0);
 
 -- --------------------------------------------------------
 
@@ -126,7 +180,29 @@ INSERT INTO `ledger_accounts` (`id`, `name`, `freezed`, `company_id`, `supplier_
 (7, 'dyuhlvfujfwuihd', 1, 1, 2, 0),
 (8, 'jsan', 0, 1, 3, 0),
 (9, 'aaaaaaaaaaaaaaa', 1, 1, 0, 0),
-(10, 'ffffffff', 1, 1, 0, 0);
+(10, 'ffffffff', 1, 1, 0, 0),
+(11, 'sadas', 0, 1, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nature_of_groups`
+--
+
+CREATE TABLE `nature_of_groups` (
+  `id` int(10) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `nature_of_groups`
+--
+
+INSERT INTO `nature_of_groups` (`id`, `name`) VALUES
+(1, 'Assets'),
+(2, 'Liabilities'),
+(3, 'Income'),
+(4, 'Expenses');
 
 -- --------------------------------------------------------
 
@@ -152,7 +228,8 @@ INSERT INTO `suppliers` (`id`, `name`, `mobile`, `email`, `address`, `freezed`, 
 (2, 'dyuhlvfujfwuihd', '4618965384', 'asda@wfeowi.com', 'foiewfnqewks', 1, 1),
 (3, 'jsan', 'lkj', 'lk@lkjlklk.dfds', 'sdsad', 0, 1),
 (4, 'aaaaasssss', '7894561237', 'ddddd@gmaail.com', 'ytrftyy', 1, 1),
-(5, 'aaaaaaaaaaaaaaa', '7987897989', 'avgzv@bhvbh.com', 'yvghh', 0, 1);
+(5, 'aaaaaaaaaaaaaaa', '7987897989', 'avgzv@bhvbh.com', 'yvghh', 0, 1),
+(6, 'sadas', '', '', '', 0, 1);
 
 -- --------------------------------------------------------
 
@@ -181,6 +258,12 @@ INSERT INTO `users` (`id`, `name`, `username`, `password`, `company_id`) VALUES
 --
 
 --
+-- Indexes for table `accounting_groups`
+--
+ALTER TABLE `accounting_groups`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `companies`
 --
 ALTER TABLE `companies`
@@ -199,9 +282,21 @@ ALTER TABLE `items`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `ledgers`
+--
+ALTER TABLE `ledgers`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `ledger_accounts`
 --
 ALTER TABLE `ledger_accounts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `nature_of_groups`
+--
+ALTER TABLE `nature_of_groups`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -221,6 +316,11 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `accounting_groups`
+--
+ALTER TABLE `accounting_groups`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+--
 -- AUTO_INCREMENT for table `companies`
 --
 ALTER TABLE `companies`
@@ -234,17 +334,27 @@ ALTER TABLE `customers`
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `ledgers`
+--
+ALTER TABLE `ledgers`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `ledger_accounts`
 --
 ALTER TABLE `ledger_accounts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+--
+-- AUTO_INCREMENT for table `nature_of_groups`
+--
+ALTER TABLE `nature_of_groups`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `suppliers`
 --
 ALTER TABLE `suppliers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `users`
 --
