64006bbebd24c9735b5fe8bca1e1251e504e954b
Abhilash Lohar <abhilashlohar01@gmail.com>
2017-08-01 10:20:27 +0530

    wer

---
+++ src/Model/Table/InvoicesTable.php
@@ -0,0 +1,120 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+use Cake\Event\Event;
+use ArrayObject;
+/**
+ * Invoices Model
+ *
+ * @property \App\Model\Table\CustomerLedgersTable|\Cake\ORM\Association\BelongsTo $CustomerLedgers
+ * @property \App\Model\Table\SalesLedgersTable|\Cake\ORM\Association\BelongsTo $SalesLedgers
+ * @property \App\Model\Table\InvoiceRowsTable|\Cake\ORM\Association\HasMany $InvoiceRows
+ *
+ * @method \App\Model\Entity\Invoice get($primaryKey, $options = [])
+ * @method \App\Model\Entity\Invoice newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\Invoice[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Invoice|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Invoice patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\Invoice[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Invoice findOrCreate($search, callable $callback = null, $options = [])
+ */
+class InvoicesTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('invoices');
+        $this->setDisplayField('id');
+        $this->setPrimaryKey('id');
+
+        
+		$this->belongsTo('CustomerLedgers', [
+			'className' => 'Ledgers',
+			'foreignKey' => 'customer_ledger_id',
+			'propertyName' => 'customer_ledgers',
+		]);
+		$this->belongsTo('SalesLedgers', [
+			'className' => 'Ledgers',
+			'foreignKey' => 'sales_ledger_id',
+			'propertyName' => 'sales_ledgers',
+		]);
+        $this->hasMany('InvoiceRows', [
+            'foreignKey' => 'invoice_id'
+        ]);
+    }
+
+	public function beforeMarshal(Event $event, ArrayObject $data, ArrayObject $options) {
+		$data['transaction_date'] = date('Y-m-d',strtotime($data['transaction_date']));
+	}
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmpty('id', 'create');
+
+        $validator
+            ->integer('invoice_no')
+            ->requirePresence('invoice_no', 'create')
+            ->notEmpty('invoice_no');
+
+        $validator
+            ->date('transaction_date')
+            ->requirePresence('transaction_date', 'create')
+            ->notEmpty('transaction_date');
+
+        $validator
+            ->decimal('total_amount_before_tax')
+            ->requirePresence('total_amount_before_tax', 'create')
+            ->notEmpty('total_amount_before_tax');
+
+        $validator
+            ->decimal('total_cgst')
+            ->requirePresence('total_cgst', 'create')
+            ->notEmpty('total_cgst');
+
+        $validator
+            ->decimal('total_sgst')
+            ->requirePresence('total_sgst', 'create')
+            ->notEmpty('total_sgst');
+
+        $validator
+            ->decimal('total_amount_after_tax')
+            ->requirePresence('total_amount_after_tax', 'create')
+            ->notEmpty('total_amount_after_tax');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules)
+    {
+        $rules->add($rules->existsIn(['customer_ledger_id'], 'CustomerLedgers'));
+        $rules->add($rules->existsIn(['sales_ledger_id'], 'SalesLedgers'));
+
+        return $rules;
+    }
+}
